8f692780e2a154a5fab295d368d4d382c7bf7c87
engeir <eirroleng@gmail.com>
2021-08-20 10:17:58 +0200

    Add traces to the animation.

--- src/plan_a_trip_to_mars/misc/animate.py
+++ src/plan_a_trip_to_mars/misc/animate.py
@@ -24,6 +24,7 @@ class AnimatedScatter(object):
 
         # Setup the figure and axes...
         self.fig, self.ax = plt.subplots(figsize=(10, 10))
+        self.ax.set_facecolor("k")
         # Then setup FuncAnimation.
         self.ani = animation.FuncAnimation(
             self.fig, self.update, interval=5, init_func=self.setup_plot, blit=True
@@ -33,9 +34,15 @@ class AnimatedScatter(object):
         """Initial drawing of the scatter plot."""
         x, y, s, c = next(self.stream).T
 
+        # Draw all objects as a scatter plot
         self.scat = self.ax.scatter(
-            x, y, c=c, s=s, vmin=0, vmax=1, cmap="jet", edgecolor="k"
+            x, y, c=c, s=s, vmin=0, vmax=1, cmap="jet", edgecolor="w"
         )
+        # Draw their trace
+        for j, (x_, y_) in enumerate(zip(x, y)):
+            setattr(self, f"line_{j}", self.ax.plot(x_, y_)[0])
+
+        # Add text that display the simulation time
         self.txt = [
             self.ax.text(
                 0.01,
@@ -46,17 +53,30 @@ class AnimatedScatter(object):
                 ha="left",
             )
         ]
-        for i, n in enumerate(self.names):
+        # Add text that show the name of each object
+        for j, n in enumerate(self.names):
             setattr(
-                self, f"txt_{i}", self.ax.text(x[i], y[i], n, va="bottom", ha="center")
+                self,
+                f"txt_{j}",
+                self.ax.text(x[j], y[j], n, va="bottom", ha="center", c="w"),
             )
-            self.txt.append(getattr(self, f"txt_{i}"))
+            self.txt.append(getattr(self, f"txt_{j}"))
+        # Define simulation area
         self.ax.axis([-cf.SIZE, cf.SIZE, -cf.SIZE, cf.SIZE])
+
         # For FuncAnimation's sake, we need to return the artist we'll be using
         # Note that it expects a sequence of artists, thus the trailing comma.
-        return [self.scat] + self.txt
+        returns = [self.scat]
+        for j in range(len(x)):
+            returns.append(getattr(self, f"line_{j}"))
+        return returns + self.txt
 
     def prepeare_data(self):
+        """Prepare data for the data stream.
+
+        A list is filled with iterator objects, such that calling next on one of them
+        reveal the next point in the trace that should be drawn on screen.
+        """
         self.p_list = []
         for obj in self.points:
             self.p_list.append(cycle(obj.trace))
@@ -73,85 +93,36 @@ class AnimatedScatter(object):
         """Update the scatter plot."""
         data = next(self.stream)
 
-        # Set x and y data...
+        # Set x and y data ...
         self.scat.set_offsets(data[:, :2])
-        # Set sizes...
+        # Set sizes ...
         self.scat.set_sizes(300 * abs(data[:, 2]) ** 1.5 + 100)
-        # Set colors..
+        # Set colors ...
         self.scat.set_array(data[:, 3])
 
-        # Set text position ...
+        # Draw traces ...
+        for j, obj in enumerate(self.points):
+            xs = [x[0] for x in obj.trace[: i + 1]]
+            ys = [y[1] for y in obj.trace[: i + 1]]
+            line = getattr(self, f"line_{j}")
+            line.set_xdata(xs)
+            line.set_ydata(ys)
+
+        # Set text position and update simulation time ...
         for txt, x, y in zip(self.txt[1:], data[:, 0], data[:, 1]):
             txt.set_position((x, y))
-
         self.txt[0].set_text(f"Time = {i % cf.TOT_TIME}")
 
-        # We need to return the updated artist for FuncAnimation to draw..
-        # Note that it expects a sequence of artists, thus the trailing comma.
-        # return (self.scat,self.txt)
-        return [self.scat] + self.txt
+        # We need to return the updated artists for FuncAnimation to draw..
+        # Note that it expects a sequence of artists, so if we only had one artist we
+        # would add a trailing comma:
+        # return [self.scat,]
+        returns = [self.scat]
+        for j in range(len(self.points)):
+            returns.append(getattr(self, f"line_{j}"))
+        return returns + self.txt
 
 
 if __name__ == "__main__":
     a = AnimatedScatter()
     plt.show()
-
-# enter image description here
-
-# import  matplotlib.pyplot as plt
-
-# def plt_anim(*args):
-#     # w, x, y = extra_data()
-#     # with np.load("docs/assets/video_data_ions_vectorized.npz", mmap_mode="r") as f:
-#     #     w = f["w"]
-#     #     x = f["x_ax"]
-#     #     y = f["y_ax"]
-#     # if x.shape[0] != y.shape[0]:
-#     #     y = y.T
-
-#     fig, ax = plt.subplots(figsize=(12, 6))
-#     # Electrons
-#     # ax.set(xlim=(-1e-6, 6e-6), ylim=(-1.1, 1.1))
-#     # ax.set(xlim=(- 1e-5, 1.6e-4), ylim=(-1.1, 1.1))
-#     # Ions
-#     # ax.set(xlim=(-2e-6, 1e-5), ylim=(-1.1, 1.1))
-#     # ax.set(xlim=(- 1e-5, 1.6e-4), ylim=(-1.1, 1.1))
-#     # inityr = np.real(y[:, 0])
-#     # inityi = np.imag(y[:, 0])
-#     lines = []
-#     for obj in args:
-#         x = [i[0] for i in obj.trace]
-#         y = [i[1] for i in obj.trace]
-#         lines.append(
-#             ax.plot(x, y, "r--", lw=0.7, label="Real")[0]
-#         )
-#     # line1 = ax.plot(x, inityr, "r--", lw=0.7, label="Real")[0]
-#     # line2 = ax.plot(x, inityi, "b--", lw=0.7, label="Imag")[0]
-#     ax.legend()
-
-#     def draw(frame, add_colorbar):
-#         y_ = y[:, frame]
-#         y_r = np.real(y_)
-#         y_i = np.imag(y_)
-#         line1.set_ydata(y_r)
-#         line2.set_ydata(y_i)
-#         # f = w[frame] / (2 * np.pi)
-#         # title = f"f = {f:.2e}"
-#         # ax.set_title(title)
-#         return ax
-
-#     def init():
-#         return draw(0, add_colorbar=True)
-
-#     def animate(frame):
-#         return draw(frame, add_colorbar=False)
-
-#     frames = len(w)
-
-#     ani = animation.FuncAnimation(
-#         fig, animate, frames, interval=100, init_func=None, repeat=False
-#     )
-#     plt.show()
-#     # if save:
-#     #     ani.save("video_ions_zoom.mp4", writer=animation.FFMpegWriter(fps=60), dpi=400)
-#     plt.close(fig)

76065988713aa9c29dd4475eef3e571862148c27
engeir <eirroleng@gmail.com>
2021-10-27 18:47:37 +0200

    **Breaking changes**

--- src/plan_a_trip_to_mars/scenarios.py
+++ src/plan_a_trip_to_mars/scenarios.py
@@ -88,88 +88,6 @@ class BigScenario(ABC):
         plt.show()
 
 
-class MarsTransfer(BigScenario):
-    """Simulate a transition from Earth to Mars."""
-
-    # Change the default SIZE and FPS of the universe
-    SIZE = 2.5 * cf.AU
-    FPS = 24
-    TOT_TIME = 24 * 365 * 2
-
-    def _create_complete_univers(self) -> None:
-        """Implement the objects and events of our universe.
-
-        Let us create four objects; the Sun, Earth, Mars and a rocket. Then we give the
-        rocket many arbitrary kicks just to see an example of how the `kick` actually
-        works.
-        """
-
-        # Create the Sun, Earth and Mars
-        sun = uni.Planet("Sun", cf.M_sun)
-        earth = uni.Planet(
-            "Earth",
-            cf.M_earth,
-            pos=pre.Vector2D(cf.D_earth, 0),
-            vel=pre.Vector2D(0, cf.V_earth),
-        )
-        mars_angle = 47
-        mars = uni.Planet(
-            "Mars",
-            cf.M_mars,
-            pos=pre.Vector2D(cf.D_mars, 0).rotate(mars_angle),
-            vel=pre.Vector2D(0, cf.V_mars).rotate(mars_angle),
-        )
-
-        # Create a rocket
-        mars_shuttle = uni.Rocket(
-            "Go!",
-            1e4,
-            pos=pre.Vector2D(cf.D_earth, 10 * cf.R_earth),
-            vel=pre.Vector2D(0, 4.8e3 + cf.V_earth),
-        )
-
-        mars_shuttle.add_kick_event(0, 2.65e3, 250 * 24)
-
-        # Create the universe all objects should live in
-        self.my_uni.add_object(sun, earth, mars, mars_shuttle)
-        self.my_uni.set_spi(3600)
-
-    def _run_simulation(self, time) -> None:
-        """Run the simulation.
-
-        We loop through the total time given in `config.py` and update the universe at
-        each time step with the 'move()' method. Additionally we may add some more logic
-        that is checked at each time step, for example when the Earth and Mars are aligned
-        with the Sun.
-        """
-        # Let us add some code that checks whether Sun, Earth and Mars are aligned.
-        s = self.my_uni.objects[0].pos.copy()
-        e = self.my_uni.objects[1].pos.copy()
-        e = e - s
-        e_angle = math.degrees(math.atan2(e.y, e.x))
-        m = self.my_uni.objects[2].pos.copy()
-        m = m - s
-        m_angle = math.degrees(math.atan2(m.y, m.x))
-
-        # We say that if their angular positions differ with less than one degree,
-        # they are at the same place in their orbits. This will trigger more than once
-        # per passage, but it does the job. Note also that the 'spi' value greatly
-        # influences the numerical precision and also the number of times the
-        # if-statements below will trigger.
-        if abs(e_angle - m_angle) < 1:
-            print(f"Aligned at time = {time % int(self.TOT_TIME / self.FPS)}")
-            # New syntax in python >=3.9
-            # print(f"Aligned at {time = }")
-        if abs(180 - e_angle + m_angle) < 1:
-            print(
-                f"Opposite of each other at time = {time % int(self.TOT_TIME / self.FPS)}"
-            )
-            # New syntax in python >=3.9
-            # print(f"Opposite of each other at {time = }")
-
-        # Add check to see if the rocket is close to Mars ...
-
-
 class Simpl(BigScenario):
 
     SIZE = 3e3
@@ -188,40 +106,6 @@ class Simpl(BigScenario):
         self.my_uni.add_object(stone, rock)
 
 
-class Parabolic(BigScenario):
-
-    SIZE = 5 * cf.AU
-    TOT_TIME = 1e4
-    FPS = 24
-    UNIT = " days"
-
-    def _create_complete_univers(self) -> None:
-        """Simplest case."""
-        sun = uni.Planet(
-            "Sun",
-            cf.M_sun,
-            pos=pre.Vector2D(0, 0),
-            vel=pre.Vector2D(0, 0),
-        )
-        rock = uni.Rocket(
-            "Rock",
-            1e4,
-            pos=pre.Vector2D(-cf.AU, 0),
-            vel=pre.Vector2D(0, cf.V_earth),
-        )
-        rotates = 49
-        stone = uni.Planet(
-            "Stone",
-            1e4,
-            pos=pre.Vector2D(-0.5 * cf.AU, 0).rotate(rotates),
-            vel=pre.Vector2D(0, 2 * cf.V_earth).rotate(rotates),
-        )
-        v_r = (1.0 * cf.G * cf.M_sun / cf.AU) ** (1 / 2)
-        rock.add_kick_event(90, v_r, 1000)
-        self.my_uni.add_object(sun, rock, stone)
-        self.my_uni.set_spi(3600)
-
-
 class Mayhem(BigScenario):
 
     TOT_TIME = 3e4
@@ -233,6 +117,8 @@ class Mayhem(BigScenario):
         rockets = ["1", "2", "3", "4", "5"]
         m = 1e30
         origo = pre.Vector2D(0, -4 * cf.AU)
+
+        # If we wanted to to add a lot of kick events...
         poss = [
             origo + pre.Vector2D(-cf.AU, 0),
             origo + pre.Vector2D(-cf.AU, 0).rotate(245),
@@ -254,6 +140,8 @@ class Mayhem(BigScenario):
             np.array([180, 277, 10, 0, 30, 30, 45, 82, 91, 0, 0, 20]) * cf.V_earth / 100
         )
         times = np.array([180, 277, 34, 62, 300, 340, 45, 82, 91, 240, 340, 20]) * 24
+
+        # Cycling through the rockets so that the zip function is not stopped too early.
         # for o, a, v, t in zip(cycle(objs), angle, velocities, times):
         #     o.add_kick_event(a, v, int(t))
 
@@ -294,3 +182,8 @@ class Jerk(BigScenario):
         ]
         for j in jerks:
             bounce.add_kick_event(*j)
+
+    def _run_simulation(self, time: int) -> None:
+        # Print velocity every 10 time units
+        if not time % 10:
+            print(abs(self.my_uni.objects[0].vel))
--- src/plan_a_trip_to_mars/simulation.py
+++ src/plan_a_trip_to_mars/simulation.py
@@ -4,19 +4,17 @@
 from plan_a_trip_to_mars import scenarios
 
 __SCENARIOS__ = {
-    1: scenarios.MarsTransfer,
-    2: scenarios.Simpl,
-    3: scenarios.Parabolic,
-    4: scenarios.Mayhem,
-    5: scenarios.Jerk,
+    1: scenarios.Simpl,
+    2: scenarios.Mayhem,
+    3: scenarios.Jerk,
 }
 
 
 class Sim:
     def __init__(self) -> None:
         """Initialise the simulation setup."""
-        self.scenario = __SCENARIOS__[4]()
-        self.save: tuple[bool, str] = (True, "mp4")
+        self.scenario = __SCENARIOS__[3]()
+        self.save: tuple[bool, str] = (False, "mp4")
         self.trace: bool = True
 
     def run_simulation(self) -> None:

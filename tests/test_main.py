5c75c65db14e4fe217fe40898b6d75c5651c6fd2
engeir <engeir@pm.me>
2024-10-02 14:06:08 +0200

    ci(test): update tests and the accompanying workflow

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,8 @@
+"""Tests for the main module."""
+
 import plan_a_trip_to_mars.__main__ as m
 
 
-def test_main_function():
-    assert m.main() == None
+def test_main_function() -> None:
+    """Dummy test for the main function."""
+    assert m.main() is None  # noqa: S101
--- tests/test_scenarios.py
+++ tests/test_scenarios.py
@@ -1,14 +1,16 @@
+"""Tests for the implemented scenarios."""
+
 import inspect
 
 import plan_a_trip_to_mars.scenarios as s
 
 
-def test_instantiate():
+def test_instantiate() -> None:
+    """Test that all scenarios inherit from `BigScenario`."""
     for n, c in inspect.getmembers(s, inspect.isclass):
-        if c.__module__ == "plan_a_trip_to_mars.scenarios":
-            if n != "BigScenario":
-                c()
-                assert issubclass(c, s.BigScenario)
+        if c.__module__ == "plan_a_trip_to_mars.scenarios" and n != "BigScenario":
+            c()
+            assert issubclass(c, s.BigScenario)  # noqa: S101
 
 
 if __name__ == "__main__":
